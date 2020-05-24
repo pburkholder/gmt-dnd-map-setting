5e1a2caab87d3b7394ef942a18941ac1644f8b61
Peter Burkholder <peter.burkholder@gsa.gov>
2020-05-24 14:13:41 -0400

    Makes a clockwise rotated map
    with white coastlines.

--- notes.sh
+++ notes.sh
@@ -9,10 +9,13 @@ fi
 
 gmt begin flood
 #  gmt subplot begin -A+JTL+o0.1i/0 -Fs6i/3.5i -M0 -R160/20/220/30+r -JOc190/25.5/292/69/6i -X1.5i -Y1.5i -B10 
-    # rotate counter-clockwise
-  gmt subplot begin -Rg260/15/300/50+r -JOb280/25.5/300/25.5/8i
-#  gmt subplot begin -R260/15/320/50+r -JOb280/25.5/300/35.5/8i
-  gmt grdimage trim.grd -I+d -Cflood.cpt
+  gmt set MAP_ANNOT_OBLIQUE 0
+  #rotate -clockwise
+  gmt subplot begin -Rg260/15/300/50+r -JOb280/25.5/300/30.5/7i -B10g5
+  #rotate anti-clockwise
+  #  gmt subplot begin -Rg260/15/300/50+r -JOb280/25.5/300/20.5/7i -B10g5
+  gmt grdimage trim.grd -I+d -Cflood.cpt -B
+  gmt coast -Wwhite -Cblue -Sblack
 gmt end show
 
 #gmt begin preflood  
