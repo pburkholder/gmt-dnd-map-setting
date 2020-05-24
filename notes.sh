db5b6787ee0fb1f650e67cd2abc2ef97ad7cd9f5
Peter Burkholder <peter.burkholder@gsa.gov>
2020-05-24 14:23:54 -0400

    Good whole area map

--- notes.sh
+++ notes.sh
@@ -11,11 +11,11 @@ gmt begin flood
 #  gmt subplot begin -A+JTL+o0.1i/0 -Fs6i/3.5i -M0 -R160/20/220/30+r -JOc190/25.5/292/69/6i -X1.5i -Y1.5i -B10 
   gmt set MAP_ANNOT_OBLIQUE 0
   #rotate -clockwise
-  gmt subplot begin -Rg260/15/300/50+r -JOb280/25.5/300/30.5/7i -B10g5
+  gmt subplot begin -Rg270/15/300/55+r -JOb280/25.5/300/30.5/7i -B10g5
   #rotate anti-clockwise
   #  gmt subplot begin -Rg260/15/300/50+r -JOb280/25.5/300/20.5/7i -B10g5
   gmt grdimage trim.grd -I+d -Cflood.cpt -B
-  gmt coast -Wwhite -Cblue -Sblack
+  gmt coast -Cblue -Sblack # -N2 -Wgray 
 gmt end show
 
 #gmt begin preflood  
